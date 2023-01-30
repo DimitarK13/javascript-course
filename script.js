dc51f0ac22629a2c42ca997a1e8d35cfd7ebe7e6
Dimitar <dimikalapocev@gmail.com>
2023-01-30 19:57:35 +0100

    First Lecture Code

---
+++ script.js
@@ -0,0 +1,54 @@
+// alert('Hello World');
+// alert(firstName);
+
+// const firstName = prompt("What's your name");
+
+// if (confirm('Do you know JavaScript')) {
+//   console.log('Hope you enjoy it ' + firstName);
+// } else {
+//   console.log('Welcome to JavaScript ' + firstName);
+// }
+
+// const firstName = 'Dimitar';
+// const number = 24;
+// const booleanTrue = true;
+// const booleanFalse = false;
+// let undef;
+// let undef2 = null;
+
+// undef = 'new value';
+
+// firstName = 'Dance';
+
+// console.log(typeof undef2);
+
+// let firstName = 'Dimitar';
+// let lastName = 'Kalapocev';
+// let age = 2023 - 2003;
+// age += 10;
+// age *= 3;
+// age /= 2;
+// age **= 2;
+// age++;
+// age--;
+
+// console.log(
+//   'I am ' +
+//     firstName +
+//     '. My last name is ' +
+//     lastName +
+//     '. I am ' +
+//     age +
+//     ' years old'
+// );
+
+// I am Dimitar. My last name is Kalapocev. I am 20 years old.
+
+// let isLegal = 18 >= 18;
+
+// let isUnder18 = 19 <= 18;
+
+// let is18 = 18 === '18';
+// let not18 = 18 !== '18';
+
+// console.log(not18);
