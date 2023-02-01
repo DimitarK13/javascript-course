44cfaad242f4f14fe79574a3dbb15993bb39d021
Dimitar <dimikalapocev@gmail.com>
2023-02-01 20:08:19 +0100

    Second Lecture

--- script.js
+++ script.js
@@ -52,3 +52,116 @@
 // let not18 = 18 !== '18';
 
 // console.log(not18);
+
+// let x = 10;
+// let y = 5;
+
+// if (x > y) {
+//   console.log('X is bigger than Y');
+// } else {
+//   console.log('X is not bigger than Y');
+// }
+
+// const myAge = 18;
+
+// if (myAge > 18) {
+//   console.log('Ti si polnoleten');
+// } else if (myAge === 18) {
+//   console.log('Ti si 18');
+// } else {
+//   console.log('Ti si maloleten');
+// }
+
+// TEAM 1 : (15, 28, 21);
+// TEAM 2 : (13, 28, 25);
+
+// TEAM 1 : (8, 33, 16);
+// TEAM 2 : (12, 28, 17);
+
+// TEAM 1 : (13, 22, 31);
+// TEAM 2 : (12, 29, 20);
+
+// let team1Avg = (13 + 22 + 31) / 3;
+// let team2Avg = (12 + 29 + 20) / 3;
+
+// if (team1Avg > team2Avg) {
+//   console.log('Team 1 Wins');
+// } else if (team1Avg === team2Avg) {
+//   console.log('Draw');
+// } else {
+//   console.log('Team 2 Wins');
+// }
+
+// let x = 15;
+// let y = 17;
+// let z = 16;
+
+// if (!'') {
+//   console.log('True');
+// } else {
+//   console.log('false');
+// }
+
+// 0, '', undefined, null, false;
+
+// let c;
+
+// if (x > y) {
+//   c = x;
+// } else {
+//   c = y;
+// }
+
+// console.log(c);
+
+// let x = 5;
+// let y = 7;
+// let z = x > y ? x : y;
+
+// console.log(z);
+
+// let myAge = 19;
+// let h;
+
+// if (myAge >= 18) {
+//   h = 'You can drive';
+// } else {
+//   h = "You can't drive";
+// }
+
+// let msg = myAge >= 18 ? 'You can drive' : "You can't drive";
+
+// console.log(h);
+// console.log(msg);
+
+// function hello() {
+//   console.log('Hello');
+// }
+
+// const hello2 = function () {
+//   console.log('Hello 2');
+// };
+
+// hello();
+// hello2();
+
+// const calcArea = function (width, height) {
+//   return width * height;
+// };
+
+// let area = calcArea(4, 3);
+// let area2 = calcArea(5, 4);
+
+// console.log(area);
+
+// mass / height(2) BMI CALCULATOR
+
+// function bmiCalculator(mass, height) {
+//   return mass / height ** 2;
+// }
+
+// let myBmi = bmiCalculator(74, 1.85);
+// let aleksandarsBmi = bmiCalculator(71, 1.81);
+// let randomBmi = bmiCalculator(120, 2.24);
+
+// console.log(myBmi, aleksandarsBmi, randomBmi);
