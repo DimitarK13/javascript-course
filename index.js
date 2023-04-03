3491db43718361b11e13cc3a6e6c3bfa0adf51e8
Dimitar <dimikalapocev@gmail.com>
2023-04-03 20:33:48 +0200

    Day 1

---
+++ index.js
@@ -0,0 +1,28 @@
+// alert('Hello World');
+
+// const prvoIme = prompt('What"s your name');
+// alert(prvoIme);
+
+// console.log(prvoIme);
+
+// if (confirm('Dali ste preku 18?')) {
+//   console.log('Vie ste preku 18');
+// } else {
+//   console.log('Vie nemate 18!');
+// }
+
+// const firstName = 'Dimitar';
+// let lastName = 'Kalapocev';
+
+// lastName = 'Kalapoceva';
+
+// console.log(firstName, lastName);
+
+const string = `What's your name`;
+const number = 23.12;
+const boolean = true;
+let undef;
+let undef2 = null;
+
+console.log(string, number, boolean, undef, undef2);
+console.log(typeof undef2);
