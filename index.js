cac355a3c02148e223215af5106b2c16c8ee18b9
Dimitar <dimikalapocev@gmail.com>
2023-04-05 20:31:46 +0200

    Day 2

--- index.js
+++ index.js
@@ -18,11 +18,121 @@
 
 // console.log(firstName, lastName);
 
-const string = `What's your name`;
-const number = 23.12;
-const boolean = true;
-let undef;
-let undef2 = null;
+// const string = `What's your name`;
+// const number = 23.12;
+// const boolean = true;
+// let undef;
+// let undef2 = null;
 
-console.log(string, number, boolean, undef, undef2);
-console.log(typeof undef2);
+// console.log(string, number, boolean, undef, undef2);
+// console.log(typeof undef2);
+
+// let x = 10 + 20; // 30
+// let godini = (2023 - 2003) * 2;
+
+// console.log(x);
+// console.log(godini);
+
+// let x = 10;
+// let y = 20;
+
+// console.log(x, y);
+
+// x += y; // x = x + y
+
+// console.log(x);
+
+// x -= y; // x = x - y;
+
+// console.log(x);
+
+// let nad18 = 18 >= 18;
+// let pod18 = 18 < 18;
+
+// console.log(nad18, pod18);
+
+// let ednakvi = 18 === '18';
+// let neEdnakvi = 18 !== 20;
+
+// let ednakvi2 = 18 == '18'; // NE E PREPORACLIVO
+// let neEdnakvi2 = 18 != '18'; // NE E PREPORACLIVO
+
+// console.log(ednakvi, neEdnakvi);
+
+// console.log(ednakvi2, ednakvi);
+
+// let ime = `Dimitar`;
+// let pozdrav = `Zdravo, moeto ime e ` + ime + `.`;
+
+// console.log(pozdrav);
+
+// let firstName = `Dimitar`;
+// let lastName = `Kalapocev`;
+// let age = 19;
+
+// My name is firstName. My last name is lastName. I am age years old.
+
+// console.log(
+//   `My name is ` +
+//     firstName +
+//     `. My last name is ` +
+//     lastName +
+//     `. I am ` +
+//     age +
+//     ` years old.`
+// );
+
+// if (5 > 2) {
+//   console.log(`5 e pogolem od 2`);
+// } else {
+//   console.log(`5 ne e pogolem od 2`);
+// }
+
+// if (age >= 18) {
+//   console.log(`Ti si polnoleten`);
+// } else {
+//   console.log(`Ti si maloleten`);
+// }
+
+// let age = 18;
+
+// if (age > 18) {
+//   console.log(`Ti si polnoleten`);
+// } else if (age === 18) {
+//   console.log(`Ti si polnoleten`);
+// } else {
+//   console.log(`Ti si maloleten`);
+// }
+
+// TIM 1: 15, 18, 21
+// TIM 2: 13, 28, 25
+
+// let avgTeam1 = (15 + 18 + 21) / 3;
+// let avgTeam2 = (13 + 28 + 25) / 3;
+
+// if (avgTeam1 > avgTeam2) {
+//   console.log(`Tim 1 e pobednik`);
+// } else if (avgTeam1 === avgTeam2) {
+//   console.log(`Rezultatot e ednakov`);
+// } else {
+//   console.log(`Tim 2 e pobednik`);
+// }
+
+// const x = 30;
+// const y = 20;
+
+// const z = x > y ? `Uslovot ni e tocen` : `Uslovot ne e tocen`;
+
+// console.log(z);
+
+// let age = 15;
+
+// if (age >= 18) {
+//   console.log(`Vie ste polnoleten`);
+// } else {
+//   console.log(`Vie ste maloleten`);
+// }
+
+// let message = age >= 18 ? `Vie ste polnoleten` : `Vie ste maloleten`;
+
+// console.log(message);
