62bfd011bf9eeb01e33302277781148b47b918a5
Dimitar <dimikalapocev@gmail.com>
2023-06-21 20:47:18 +0200

    Day 1

---
+++ index.js
@@ -0,0 +1,16 @@
+// ova e komentar vo eden red
+/* 
+ova e
+komentar
+na poveke
+redovi
+*/
+
+// alert function that prints 'Hello World'
+alert('Hello World');
+
+// Getting results from user and storing in variable
+let ime = prompt('Kako se vikas');
+
+// Printing name from user
+console.log(ime);
