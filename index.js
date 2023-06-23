422298b8d2af933b05afa8309bf1d624db7bdf5a
Dimitar <dimikalapocev@gmail.com>
2023-06-23 20:26:29 +0200

    Day 2

--- index.js
+++ index.js
@@ -6,11 +6,167 @@ na poveke
 redovi
 */
 
-// alert function that prints 'Hello World'
-alert('Hello World');
+// // alert function that prints 'Hello World'
+// alert('Hello World');
 
-// Getting results from user and storing in variable
-let ime = prompt('Kako se vikas');
+// // Getting results from user and storing in variable
+// let ime = prompt('Kako se vikas');
 
-// Printing name from user
-console.log(ime);
+// // Printing name from user
+// console.log(ime);
+
+// var ime = 'Dimitar';
+// let godini = 20;
+// const prezime = 'Kalapocev';
+
+// console.log(ime, godini, prezime);
+
+// godini = 21;
+// prezime = 'Kalapoceva';
+
+// console.log(godini, prezime);
+
+// // declaring strings
+// let ime = 'Dimitar';
+// let ime2 = 'Dimitar';
+// let ime3 = `Dimitar ' " `;
+// // let godini = '20';
+
+// // declaring numbers
+// let godini = 20;
+
+// // Getting numbers with calculation
+// let godini1 = 15 + 5;
+
+// // declaring booleans
+// let tocno = true;
+// let neTocno = false;
+
+// // Getting boolean with calcs
+// let daliEPogolemo = 15 > 5;
+
+// // declaring undefined
+// let promenliva;
+
+// console.log(ime3, godini, godini1, daliEPogolemo, promenliva);
+// console.log(typeof godini1);
+
+// Arithmetic operators
+// let broj = 10;
+// broj++;
+// console.log(broj);
+
+// let broj2 = 10;
+// broj2--;
+// console.log(broj2);
+
+// console.log(10 + 5);
+// console.log(10 - 5);
+// console.log(10 * 2);
+// console.log(10 / 2);
+// console.log(10 ** 2);
+// console.log(10 % 3);
+
+// let x = 10;
+// let y = 5;
+// // x = x + y;
+// x += y; // x = x + y;
+// x -= y; // x = x - y;
+// x *= y; // x = x * y;
+// x **= y; // x = x ** y;
+// x /= y; // x = x / y;
+// x %= y; // x = x % y;
+
+// console.log(x);
+
+// let x = 20;
+// let y = '20';
+
+// console.log(x == y);
+// console.log(x === y);
+
+// let m = 15;
+// let n = '15';
+
+// console.log(m != n);
+// console.log(m !== n);
+
+// let a = 10;
+// let b = 15;
+
+// console.log(a > b);
+// console.log(a < b);
+
+// let ime = `Dimitar`;
+// let prezime = `Kalapocev`;
+// let godini = 20;
+
+// console.log(ime + ' so prezime . ' + prezime + ' e ' + 20 + ' godini.');
+
+// // Moeto ime e Dimitar. Moeto prezime e Kalapocev. Imam 20 godini.
+
+// console.log(
+//   `Moeto ime e ` +
+//     ime +
+//     `. Moeto prezime e ` +
+//     prezime +
+//     `. Imam ` +
+//     godini +
+//     ` godini.`
+// );
+
+// console.log(
+//   `Moeto ime e ` +
+//     ime +
+//     `. Moeto prezime e ` +
+//     prezime +
+//     `. Imam ` +
+//     godini +
+//     ` godini.`
+// );
+
+// if (5 > 2) {
+//   console.log('5 e pogolemo od 2');
+// }
+
+// if (5 < 2) {
+//   console.log('True');
+// } else {
+//   console.log('5 ne e pomalo od 2');
+// }
+
+// let godini = 18;
+
+// godini > 18;
+
+// if (godini >= 18) {
+//   console.log('Polnoleten si');
+// } else {
+//   console.log('Maloleten si');
+// }
+
+// Tim 1 = (15, 18, 21), (8, 33, 16), (13, 22, 31)
+// Tim 2 = (13, 28, 25), (12, 28, 27), (12, 29, 20)
+
+// let prosekTim1 = (15 + 18 + 21) / 3;
+// let prosekTim2 = (13 + 28 + 25) / 3;
+
+// if (prosekTim1 > prosekTim2) {
+//   console.log('Tim 1 e pobednik');
+// } else if (prosekTim1 === prosekTim2) {
+//   console.log('Rezultatite se ednakvi');
+// } else {
+//   console.log('Tim 2 e pobednik');
+// }
+
+false;
+0;
+('');
+undefined;
+null;
+
+if (null) {
+  console.log('True');
+} else {
+  console.log('False');
+}
