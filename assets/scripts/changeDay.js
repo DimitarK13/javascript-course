2da3a83356f6ed2223b9cfd0e5e535ce30b7b924
Dimitar <dimikalapocev@gmail.com>
2023-09-16 16:39:25 +0200

    Fix program display

--- assets/scripts/changeDay.js
+++ assets/scripts/changeDay.js
@@ -1,25 +1,83 @@
 const buttons = document.querySelectorAll('.select-day-btn');
-const days = document.querySelectorAll('.day-agenda-item');
+let programHeading = document.querySelector('.day-agenda__heading');
+let programList = document.querySelector('.day-agenda__list');
 
 for (let i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener('click', () => {
-    resetButtons();
+    reset();
     buttons[i].classList.add('active');
     let button_id = buttons[i].getAttribute('id');
-    let selectedDay = document.getElementById(`${button_id}-item`);
 
-    for (let i = 0; i < days.length; i++) {
-      days[i].classList.remove('opened');
-    }
+    programHeading.textContent = programDays[button_id].title;
+    programDays[button_id].program.map((item) => {
+      const itemEl = document.createElement('li');
+      itemEl.textContent = item;
 
-    selectedDay.classList.add('opened');
+      programList.appendChild(itemEl);
+    });
   });
 }
 
-function resetButtons() {
+function reset() {
+  programList.innerHTML = '';
+  programHeading.innerHTML = '';
   for (let i = 0; i < buttons.length; i++) {
     buttons[i].classList.remove('active');
   }
 }
 
-resetButtons();
+reset();
+
+const programDays = {
+  'day-1': {
+    title: 'Ден 1',
+    program: [
+      'Запознавање',
+      'Што ќе учиме',
+      'Поставување VS Code',
+      'Што е JavaScript',
+      "'Hello World'",
+      'Коментари',
+    ],
+  },
+  'day-2': {
+    title: 'Ден 2',
+    program: [
+      'Вредности',
+      'Променливи',
+      'Оператори',
+      'If... Else',
+      'Точни и неточни вредности',
+    ],
+  },
+  'day-3': {
+    title: 'Ден 3',
+    program: ['For Loop', 'While Loop', 'Loop вежби'],
+  },
+  'day-4': {
+    title: 'Ден 4',
+    program: ['Функции', 'Функции вежби'],
+  },
+  'day-5': {
+    title: 'Ден 5',
+    program: ['Низи', 'Методи на низи', 'Низи вежби'],
+  },
+  'day-6': {
+    title: 'Ден 6',
+    program: ['Објекти', "'this' keyword", 'Објекти вежби'],
+  },
+  'day-7': {
+    title: 'Ден 7',
+    program: ['ДОМ - Document Object Model'],
+  },
+  'day-8': {
+    title: 'Ден 8',
+    program: [
+      'Посветување внимание на несигурни теми',
+      'Вежби',
+      'Прашања',
+      'Што следно',
+      'Корисни извори',
+    ],
+  },
+};
