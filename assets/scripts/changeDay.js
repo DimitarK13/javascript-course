ecc4e6a38fa477bda227ef6a6809399ac78f4f52
Dimitar <dimikalapocev@gmail.com>
2023-09-17 17:51:28 +0200

    Add logic for presentation

--- assets/scripts/changeDay.js
+++ assets/scripts/changeDay.js
@@ -1,18 +1,42 @@
+import programDays from './programDays.js';
+import presentationData from './presentationData.js';
+
 const buttons = document.querySelectorAll('.select-day-btn');
 let programHeading = document.querySelector('.day-agenda__heading');
 let programList = document.querySelector('.day-agenda__list');
+let presentationCode = document.querySelector('.presentation__code');
+let presentationContent = document.querySelector('.presentation__content');
+let presentationHeading = document.querySelector('.presentation__heading');
+let presentationImportant = document.querySelector('.presentation__important');
+
+programList.addEventListener('click', (e) => {
+  const isClickable = e.target.getAttribute('class') === 'clickable';
+  if (!isClickable) return;
+
+  const itemId = e.target.getAttribute('id');
+  resetPresentation();
+
+  const { heading, content, code, important } = presentationData[itemId];
+  if (heading) presentationHeading.innerHTML = heading;
+  if (content) presentationContent.innerHTML = content;
+  if (code) presentationCode.innerHTML = code;
+  if (important) presentationImportant.innerHTML = important;
+});
 
 for (let i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener('click', () => {
     reset();
     buttons[i].classList.add('active');
-    let button_id = buttons[i].getAttribute('id');
+    let buttonId = buttons[i].getAttribute('id');
 
-    programHeading.textContent = programDays[button_id].title;
-    programDays[button_id].program.map((item) => {
+    programHeading.textContent = programDays[buttonId].title;
+    programDays[buttonId].program.map((item) => {
       const itemEl = document.createElement('li');
       itemEl.textContent = item.content;
-      if (item.id) itemEl.setAttribute('id', item.id);
+      if (item.id) {
+        itemEl.setAttribute('id', item.id);
+        itemEl.setAttribute('class', 'clickable');
+      }
 
       programList.appendChild(itemEl);
     });
@@ -29,79 +53,11 @@ function reset() {
 
 reset();
 
-const programDays = {
-  'day-1': {
-    title: 'Ден 1',
-    program: [
-      { content: 'Запознавање', id: 'introduction' },
-      { content: 'Што ќе учиме' },
-      { content: 'Поставување VS Code' },
-      { content: 'Што е JavaScript', id: 'what-is-js' },
-      { content: "'Hello World'", id: 'hello-world' },
-      { content: 'Коментари', id: 'comments' },
-    ],
-  },
-  'day-2': {
-    title: 'Ден 2',
-    program: [
-      { content: 'Вредности', id: 'values' },
-      { content: 'Променливи', id: 'variables' },
-      { content: 'Оператори', id: 'operators' },
-      { content: 'If... Else', id: 'if-else' },
-      { content: 'Точни и неточни вредности', id: 'true-falsy-values' },
-    ],
-  },
-  'day-3': {
-    title: 'Ден 3',
-    program: [
-      { content: 'Loops', id: 'loops' },
-      { content: 'For Loop', id: 'for-loop' },
-      { content: 'While Loop', id: 'while-loop' },
-      { content: 'Loop вежби' },
-    ],
-  },
-  'day-4': {
-    title: 'Ден 4',
-    program: [
-      { content: 'Функции', id: 'functions' },
-      { content: 'Функции вежби' },
-    ],
-  },
-  'day-5': {
-    title: 'Ден 5',
-    program: [
-      { content: 'Низи', id: 'arrays' },
-      { content: 'Методи на низи', id: 'array-methods' },
-      { content: 'Низи вежби' },
-    ],
-  },
-  'day-6': {
-    title: 'Ден 6',
-    program: [
-      { content: 'Објекти', id: 'objects' },
-      { content: "'this' keyword", id: 'this-keyword' },
-      { content: 'Објекти вежби' },
-    ],
-  },
-  'day-7': {
-    title: 'Ден 7',
-    program: [{ content: 'ДОМ - Document Object Model', id: 'dom' }],
-  },
-  'day-8': {
-    title: 'Ден 8',
-    program: [
-      { content: 'Посветување внимание на несигурни теми' },
-      { content: 'Вежби' },
-      { content: 'Прашања' },
-      { content: 'Што следно', id: 'what-next' },
-      { content: 'Корисни извори', id: 'useful-sources' },
-    ],
-  },
-};
+function resetPresentation() {
+  presentationHeading.innerHTML = '';
+  presentationContent.innerHTML = '';
+  presentationCode.innerHTML = '';
+  presentationImportant.innerHTML = '';
+}
 
-document.querySelector(
-  '.presentation__code'
-).innerHTML = `for (let i = 0; i < 10; i++) {<br>
-  &nbsp;&nbsp;console.log(‘Printed ‘ + i + ‘ times’);
-<br>}
-`;
+resetPresentation();
