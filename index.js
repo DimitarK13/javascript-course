6bb4d36192b8bdfa71d32526b0b89f1391d5a0b3
Dimitar <dimikalapocev@gmail.com>
2023-09-28 20:08:27 +0200

    Day 1

---
+++ index.js
@@ -0,0 +1,14 @@
+// alert('Dimitar');
+console.log('Hello World - says Dimitar');
+console.log('This is a second console log');
+
+// ova e single line comment
+
+/* 
+    fwbahjfwk
+    awdfgwikfewwef
+    fewewewfe
+    vewfew
+*/
+
+// Den 1 zavrsuva ovde
