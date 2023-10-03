235521437207b7a058af474baa82f64e6d470bcd
Dimitar <dimikalapocev@gmail.com>
2023-10-03 20:12:19 +0200

    Day 2

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // alert('Dimitar');
-console.log('Hello World - says Dimitar');
-console.log('This is a second console log');
+// console.log('Hello World - says Dimitar');
+// console.log('This is a second console log');
 
 // ova e single line comment
 
@@ -12,3 +12,190 @@ console.log('This is a second console log');
 */
 
 // Den 1 zavrsuva ovde
+
+// // Pisuvanje na strings
+// console.log(typeof 'Dimitar 23 ;dae@!.');
+// console.log(typeof '23');
+
+// // Pisuvanje na brojki
+// console.log(typeof 23);
+// console.log(23, 5);
+// console.log(23.5);
+
+// // Pisuvanje na boolean
+// console.log(typeof true);
+// console.log(false);
+// // Dobivanje booleans preku sporedbi
+// console.log(typeof (23 > 15));
+// console.log(23 < 15);
+
+// // Dobivanje 'undefined' i 'null'
+// console.log(typeof undefined);
+// console.log(typeof null);
+
+// console.log('Dimitar"s hungry');
+// console.log("Dimitar's hungry");
+// console.log(`Dimitar's quote is "Hello World"`);
+
+// // Deklariranje na promenlivi so const
+// // Promenlivi koi ne se menuvaat
+// const ime = 'Dimitar';
+// const prezime = 'Kalapocev';
+// console.log('Dimitar', 'Kalapocev');
+
+// // ime = 'Dimitra'; Menuvanje na promenliva so const
+
+// console.log(ime, prezime);
+
+// // Deklariranje na promenlivi so let
+// // Promenlivi koi se menuvaat
+// let godini = 20;
+// console.log(godini);
+
+// godini = 21; // menuvanje na promenliva so let
+// console.log(godini);
+
+// // Vistinskata pricina za template literals
+// const ime = 'Dimitar';
+// const prezime = 'Kalapocev';
+
+// console.log('My name is ' + ime + ' ' + prezime);
+// console.log(`My name is ${ime} ${prezime}`);
+
+// // Sobiranje +
+// let rezultat = 20 + 10;
+// console.log(rezultat);
+
+// // Odzemanje -
+// rezultat = 20 - 10;
+// console.log(rezultat);
+
+// // Mnozenje *
+// rezultat = 20 * 2;
+// console.log(rezultat);
+
+// // Delenje /
+// rezultat = 20 / 2;
+// console.log(rezultat);
+
+// // Eksponencijacija **
+// rezultat = 2 ** 5;
+// console.log(rezultat);
+
+// // Modul (ostatok pri delenje) %
+// rezultat = 10 % 3;
+// console.log(rezultat);
+// rezultat = 20 % 4;
+// console.log(rezultat);
+
+// // Operatori za sporeduvanje > < <= >= === !==
+// let rezultat = 23 > 15; // True
+// console.log(rezultat);
+
+// rezultat = 23 < 15; // False
+// console.log(rezultat);
+
+// rezultat = 15 <= 15; // True
+// console.log(rezultat);
+
+// rezultat = 15 >= 15; // True
+// console.log(rezultat);
+
+// rezultat = 15 === 15; // True
+// console.log(rezultat);
+
+// rezultat = 15 === '15'; // False
+// console.log(rezultat);
+
+// rezultat = 15 !== 14; // True
+// console.log(rezultat);
+
+// // Ednostaven if... else statement so 2 broja
+// if (5 > 5) {
+//   console.log(`5 e pogolemo od 2`);
+// } else if (5 === 5) {
+//   console.log(`5 e ednakvo na 5`);
+// } else {
+//   console.log(`5 e pomalo od 7`);
+// }
+
+// let godini = 18;
+
+// if (godini >= 18) {
+//   console.log('Polnoleten. Slobodno prodolzi.');
+// } else {
+//   console.log('Maloleten. Povikaj roditel.');
+// }
+
+// VEZBA 1 - POGOLEMO ILI POMALOs
+// Tim 1: (15, 18, 21)
+// Tim 2: (13, 28, 25)
+
+// const prosek1 = (15 + 18 + 21) / 3;
+// const prosek2 = (13 + 28 + 25) / 3;
+
+// if (prosek1 > prosek2) {
+//   console.log(`Pobednik e Tim 1`);
+// } else if (prosek1 === prosek2) {
+//   console.log(`Rezultat e izednacen`);
+// } else {
+//   console.log(`Pobednik e Tim 2`);
+// }
+
+// Falsy values
+if (0) {
+  console.log('True');
+} else {
+  console.log('False');
+}
+
+if (false) {
+  console.log('True');
+} else {
+  console.log('False');
+}
+
+if ('') {
+  console.log('True');
+} else {
+  console.log('False');
+}
+
+if (undefined) {
+  console.log('True');
+} else {
+  console.log('False');
+}
+
+if (null) {
+  console.log('True');
+} else {
+  console.log('False');
+}
+
+if (NaN) {
+  console.log('True');
+} else {
+  console.log('False');
+}
+
+// Truthy values
+if (1) {
+  console.log('True');
+} else {
+  console.log('False');
+}
+
+if ('a') {
+  console.log('True');
+} else {
+  console.log('False');
+}
+
+if (true) {
+  console.log('True');
+} else {
+  console.log('False');
+}
+
+// DEN 2 ZAVRSUVA OVDE
